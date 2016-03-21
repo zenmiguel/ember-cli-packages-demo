fa8f768428ce6e020d41ee2e1d13b882243cccae
Miguel Madero <git@miguelmadero.com>
2016-03-21 10:48:43 -0300

    Cleans approvals file

--- approvals/test.js
+++ approvals/test.js
@@ -19,25 +19,17 @@ describe('Dist contents for a dev builds', function () {
   it('should contain an index file', function () {
     this.verifyFileContent('.approvals-dist/index.html');
   });
-  it('contains package1.js', function () {
-    this.verifyFileContent('.approvals-dist/assets/package1.js');
-  });
-  it('contains package1.css', function () {
-    this.verifyFileContent('.approvals-dist/assets/package1.css');
-  });
-  it('contains package2.js', function () {
-    this.verifyFileContent('.approvals-dist/assets/package2.js');
-  });
-  it('contains package1.js', function () {
-    this.verifyFileContent('.approvals-dist/assets/package1.js');
-  });
-  it('contains package2.css', function () {
-    this.verifyFileContent('.approvals-dist/assets/package2.css');
-  });
-  it('contains ember-cli-packages-demo.js', function () {
-    this.verifyFileContent('.approvals-dist/assets/ember-cli-packages-demo.js');
-  });
-  it('contains ember-cli-packages-demo.css', function () {
-    this.verifyFileContent('.approvals-dist/assets/ember-cli-packages-demo.css');
+
+  var assetsToVerify = ['package1.js',
+    'package1.css',
+    'package2.js',
+    'package1.js',
+    'package2.css',
+    'ember-cli-packages-demo.js',
+    'ember-cli-packages-demo.css'];
+  assetsToVerify.forEach(function (file) {
+    it(`contains ${file}`, function () {
+      this.verifyFileContent(`.approvals-dist/assets/${file}`);
+    });
   });
 });
